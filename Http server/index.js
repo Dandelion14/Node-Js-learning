228ef297b2e2980b7bcdabac2a10953268eefd10
Deepali Panchal <138522108+Dandelion14@users.noreply.github.com>
2026-01-29 15:57:40 +0530

    Enhance server to log requests to log_data.txt
    Updated HTTP server to log requests and responses to a file.

--- Http server/index.js
+++ Http server/index.js
@@ -1,7 +1,22 @@
-const { log } = require("console")
-let htt= require("http")
-let server=htt.createServer((req , res)=>{
-    console.log("server here!")
-    res.end("yes server")
-});
-server.listen("443")
+const fs = require("fs")
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    console.log(req.headers)
+
+    const log = `${Date.now()} ${req.url} new request received!\n`
+
+    fs.appendFile("log_data.txt", log, (err) => {
+        if (err) {
+            res.statusCode = 500
+            return res.end("logging failed")
+        }
+
+        res.end("yes worked")
+        console.log(req.url)
+    })
+})
+
+server.listen(3000, () => {
+    console.log("server running on port 3000")
+})
